e2db1f0dd5048b81d34601f8eef33aaf37db21ce
BRYAN RODRIGUEZ <bryanRod01@outlook.com>
2023-07-02 17:53:51 -0700

    Added new models for Blog topic and entry

--- blog_logs/admin.py
+++ blog_logs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import BlogTopic, BlogEntry
+
 # Register your models here.
+admin.site.register(BlogTopic)
+admin.site.register(BlogEntry)
--- blog_logs/models.py
+++ blog_logs/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class BlogTopic(models.Model):
+    """A blog tile that displays an image, title, and short entry."""
+    title = models.TextField(max_length=50)
+    text = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/')
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text
+    
+class BlogEntry(models.Model):
+    """The entry of a specific blog topic."""
+    topic = models.ForeignKey(BlogTopic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "entries"
+
+    def __str__(self):
+        """Return a simple string representing the entry."""
+        return f"{self.text[:50]}..."
