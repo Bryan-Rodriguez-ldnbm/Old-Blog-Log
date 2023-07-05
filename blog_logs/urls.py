dc304cd292e6ef0498ad59acf73abff7135cf0f3
BRYAN RODRIGUEZ <bryanRod01@outlook.com>
2023-07-05 14:46:18 -0700

    Setup basic HTML pages with views and url mappings

--- blog_logs/urls.py
+++ blog_logs/urls.py
@@ -5,7 +5,9 @@ from django.urls import path
 from . import views
 
 app_name = 'blog_logs'
-urlpatters = [
+urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    # Page that shows all the blogs
+    path('blogs/', views.topics, name='topics')
 ]
--- blog_logs/views.py
+++ blog_logs/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from .models import BlogTopic
+
 # Create your views here.
+def index(request):
+    """The home page for Learning Log."""
+    return render(request, 'blog_logs/index.html')
+
+def BlogTopic(request, topic_id):
+    """Show a single Blog and its associated Entry"""
+    blog = BlogTopic.object.get(id=topic_id)
+    entries = blog.entry_set.order_by('-date_added')
+    context = {'blog': blog, 'entries': entries}
+    return render(request, 'blog_logs/index.html')
